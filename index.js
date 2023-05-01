357298784835084a8f7a59e429e1e8f074cab9d6
Cody Roberts <pithlyx@gmail.com>
2023-05-01 11:47:10 -0600

    added math variables

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 16;
+let num4 = 6;
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = 16 % 6;
+let max = Math.max(1, 5, 9, 14, 20);
